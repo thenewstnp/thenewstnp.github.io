f669295dc06c8a5f3f8b95f980ab082a7f8ad072
LiuzZe <2411509324@qq.com>
2018-09-16 10:37:13 +0800

    添加vsCode快捷键页面

---
+++ vsCode/js/index.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: LiuZe 
+ * @Date: 2018-09-16 08:05:45 
+ * @Last Modified by: LiuZe
+ * @Last Modified time: 2018-09-16 10:19:57
+ */
+
+$(function () {
+    $(".right").css("height", $(document.body).height());
+    $(".left").css("width", $(document.body).width() - $(".right").width());
+    $(window).resize(() => {
+        $(".left").css("width", $(document.body).width() - $(".right").width());
+        $(".right").css("height", $(document.body).height());
+    })
+
+    $(".p_content").on("click", "a", function () {
+        $(this).parent().parent("li").addClass("addbackground").siblings().removeClass("addbackground");
+    })
+
+    $(document).scroll(function () {
+        if ($(document).scrollTop() <= 1000) {
+            $(".sidebar").fadeOut(300);
+        } else {
+            $(".sidebar").fadeIn(300);
+        }
+    })
+
+    $(".sidebar").click(function () {
+        $("html,body").animate({
+            scrollTop: 0,
+        }, 500);
+        $(".p_content li").removeClass("addbackground");
+    })
+})
